9d3fedb3c3411c7c22ce4317693f11e995f1a531
Will YE <iamthenewlebron@gmail.com>
2017-03-18 12:44:54 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * This is a class that tests the Deck class.
+ */
+public class Main {
+
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+		String a[]={"1","2","3","4"};
+		String b[]={"Hearts","Spades"};
+		int c[]={1,2,3,4};
+        Deck d = new Deck(a,b,c);
+    }
+}
+
